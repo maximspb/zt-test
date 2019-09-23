413d45bcedad2083f443ab89665a7164ef45121f
maximspb <maxim.kuzn@gmail.com>
2019-09-23 12:47:49 +0300

    dev: fixed configs, composer.json

--- config/db.php
+++ config/db.php
@@ -2,10 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=zt-test', //@todo: заменить на переменные при сборке
+    'username' => 'homestead', //@todo: заменить на переменные при сборке
+    'password' => 'secret', //@todo: заменить на переменные при сборке
     'charset' => 'utf8',
+    'attributes' => [PDO::MYSQL_ATTR_LOCAL_INFILE => true],
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
--- config/web.php
+++ config/web.php
@@ -14,13 +14,13 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'zt-test',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
-            'identityClass' => 'app\models\User',
+            'identityClass' => \app\models\User::class,
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
@@ -43,14 +43,13 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
@@ -68,7 +67,7 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*'],
     ];
 }
 
